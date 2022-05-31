91b588186550e241c482d49e1e530ae7ce1ad927
wbrydak <brydakwojciech@gmail.com>
2022-06-01 00:12:44 +0200

    rename + inital setup for deploying ecs cluster

--- terraform/puppet-in-tf/instance.tf
+++ terraform/puppet-in-tf/instance.tf
@@ -4,15 +4,16 @@ provider "aws" {
   region = "${var.region}"
 }
 
-resource "aws_instance" "test" {
-  ami = "ami-02584c1c9d05efa69"
-  instance_type = "t2.micro"
+resource "aws_ecr_repository" "learning-devops" {
+  name = "learning-devops"
+}
+
+resource "aws_ecs_cluster" "learning-devops" {
+  name = "learning-devops"
 }
 
 variable "aws_access_key" {}
 
 variable "aws_secret_key" {}
 
-variable "region" {
-        default = "eu-central-1"
-}
+variable "region" {}
